048c3a78de92055670ca73be6f539192251010c4
Barry Searle <bsearle@google.com>
2022-11-26 18:36:22 +0000

    private services access

--- modules/global/vpc.tf
+++ modules/global/vpc.tf
@@ -22,6 +22,22 @@ resource "google_compute_network" "consumer_vpc" {
   routing_mode            = "REGIONAL"
 }
 
+resource "google_compute_global_address" "private_ip_block" {
+  name         = "private-ip-block"
+  purpose      = "VPC_PEERING"
+  address_type = "INTERNAL"
+  ip_version   = "IPV4"
+  addresses    = "10.0.9.0"
+  prefix_length = 24
+  network       = google_compute_network.hub_vpc.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.hub_vpc.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_block.name]
+}
+
 resource "google_compute_firewall" "allow-internal" {
   name    = "${var.company}-fw-allow-internal"
   network = "${google_compute_network.hub_vpc.name}"
