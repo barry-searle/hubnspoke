773c821eadcc59098b4d0731f318de7a840fcf95
Barry Searle <bsearle@google.com>
2022-11-27 18:22:47 +0000

    trying to make it basically run

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hubnspoke
-Repo for simukating a hub and spoke network architecture.
+
+Repo for simulating a hub and spoke network architecture.
+
+#run the following in cloud shell to ensure your project has all the necessary services enabled
+gcloud services enable \
+    cloudresourcemanager.googleapis.com \
+    compute.googleapis.com \
+    iam.googleapis.com \
+    oslogin.googleapis.com \
+    servicenetworking.googleapis.com \
+    sqladmin.googleapis.com
+
--- main/main.tf
+++ main/main.tf
@@ -7,8 +7,8 @@ module "vpc" {
   source = "../modules/global" 
   env                   = "${var.var_env}"
   company               = "${var.var_company}"
-  var_var_hub_pan_subnet = "${var.hub_pan_subnet}"
-  var_var_hub_spoke1_mysql_subnet= "${var.hub_spoke1_mysql_subnet}"
+  var_hub_pan_subnet    = "${var.hub_pan_subnet}"
+  var_hub_spoke1_mysql_subnet= "${var.hub_spoke1_mysql_subnet}"
   var_consumer_mysqlclient_subnet = "${var.consumer_mysqlclient_subnet}"
 }
 
@@ -18,8 +18,8 @@ module "hub" {
   subnetwork1           = "${module.uc1.uc1_out_public_subnet_name}"
   env                   = "${var.var_env}"
   company               = "${var.var_company}"
-  var_var_hub_pan_subnet = "${var.hub_pan_subnet}"
-  var_var_hub_spoke1_mysql_subnet= "${var.var_hub_spoke1_mysql_subnet}"
+  var_hub_pan_subnet = "${var.hub_pan_subnet}"
+  var_hub_spoke1_mysql_subnet= "${var.var_hub_spoke1_mysql_subnet}"
 }
 
 module "consumer" {
@@ -29,13 +29,13 @@ module "consumer" {
   env                   = "${var.var_env}"
   company               = "${var.var_company}"
   var_consumer_mysqlclient_subnet = "${var.consumer_mysqlclient_subnet}"
-  var_ue1_private_subnet= "${var.ue1_private_subnet}"
+  #var_ue1_private_subnet= "${var.ue1_private_subnet}"
 }
 ######################################################################
 # Display Output Public Instance
 ######################################################################
-output "uc1_public_address"  { value = "${module.uc1.uc1_pub_address}"}
-output "uc1_private_address" { value = "${module.uc1.uc1_pri_address}"}
-output "ue1_public_address"  { value = "${module.ue1.ue1_pub_address}"}
-output "ue1_private_address" { value = "${module.ue1.ue1_pri_address}"}
-output "vpc_self_link" { value = "${module.vpc.out_vpc_self_link}"}
+#output "uc1_public_address"  { value = "${module.hub.uc1_pub_address}"}
+#output "uc1_private_address" { value = "${module.uc1.uc1_pri_address}"}
+#output "ue1_public_address"  { value = "${module.ue1.ue1_pub_address}"}
+#output "ue1_private_address" { value = "${module.ue1.ue1_pri_address}"}
+#output "vpc_self_link" { value = "${module.vpc.out_vpc_self_link}"}
